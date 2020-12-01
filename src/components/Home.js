b066ced0ef6640c8def43bf713d194b2544bc295
Connor Todd <connortodd0304@gmail.com>
2020-12-01 17:08:03 +0000

    implemented fetch of questions via category and difficulty

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,24 +3,49 @@ import axios from "axios";
 
 function Home() {
   const [gameCategories, setGameCategories] = React.useState([]);
+  const [selectedGameCategoryId, setSelectedGameCategoryId] = React.useState(0);
+  const [selectedDifficulty, setSelectedDifficulty] = React.useState("easy");
 
   React.useEffect(() => {
     getGameCategories();
   }, []);
 
   const getGameCategories = () => {
+    axios.get("https://opentdb.com/api_category.php").then((response) => {
+      setGameCategories(response.data.trivia_categories);
+      setSelectedGameCategoryId(response.data.trivia_categories[0].id);
+    });
+  };
+
+  const getGameInfo = () => {
     axios
-      .get("https://opentdb.com/api_category.php")
-      .then((response) => setGameCategories(response.data.trivia_categories));
+      .get(
+        `https://opentdb.com/api.php?amount=10&category=${selectedGameCategoryId}&type=multiple&difficulty=${selectedDifficulty}`
+      )
+      .then((response) => {
+        console.log(response);
+      });
   };
 
   return (
     <div>
-      {gameCategories.map((gameCategory) => (
-        <p>{gameCategory.name}</p>
-      ))}
+      {/* Game Categories dropdown */}
+      <select
+        onChange={(event) => setSelectedGameCategoryId(event.target.value)}
+      >
+        {gameCategories.map((gameCategory) => (
+          <option value={gameCategory.id}>{gameCategory.name}</option>
+        ))}
+      </select>
+      {/* Difficulty dropdown */}
+      <select onChange={(event) => setSelectedDifficulty(event.target.value)}>
+        {["easy", "medium", "hard"].map((difficulty) => (
+          <option value={difficulty}>{difficulty}</option>
+        ))}
+      </select>
+      <p>SelectedGame: {selectedGameCategoryId}</p>
       <p>Home</p>
-      <button>Randomize your game </button>
+      <button onClick={getGameInfo}>Confirm Game Choice</button>
     </div>
   );
 }
