af633ef63f1a1660024db0f8b346acc995f1b436
Connor Todd <connortodd0304@gmail.com>
2020-12-01 23:43:43 +0000

    back buttons redirecting user back to home screen

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import parse from "html-react-parser";
 import { GameContext } from '../contexts/GameContext';
 import { createAnswersList } from "../helpers/createAnswerslist";
+import Modal from "./Modal";
+
 
 function Game() {
+    const [isModalDisplayed, setModalDisplay] = React.useState(false)
+
   let { gameQuestions, points, currentQuestionId, testUserAnswer } = React.useContext(GameContext);
 
-
-  
 return (
   <div>
     <p>Game</p>
+      <button onClick={() => setModalDisplay(true)}>Go Back</button>
+
+      {isModalDisplayed && <Modal setModalDisplay={setModalDisplay} />}
+
     <p>Current Score: {points}</p>
     <p>Current Question: {currentQuestionId + 1} / 10</p>
     {gameQuestions
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import parse from "html-react-parser";
+import { History } from 'history';
 import { GameContext } from '../contexts/GameContext';
 import { createAnswersList } from '../helpers/createAnswerslist';
+import Modal from "./Modal";
 
-function Result() {
+interface ChildComponentProps {
+  history: History,
+}
+
+const Result: React.SFC<ChildComponentProps> = ({ history }) =>  {
+    const [isModalDisplayed, setModalDisplay] = React.useState(false)
     let { gameQuestions, points } = React.useContext(GameContext);
   return (
     <div>
+      <button onClick={() => setModalDisplay(true)}>Go Back</button>
+      {isModalDisplayed && <Modal setModalDisplay={setModalDisplay} />}
      <p>Result</p>
      <p>Your score was {points}</p>
      <p>You scored {points / 10} out of 10</p>
