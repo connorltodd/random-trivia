1a5bcf29c29321b9f2aa52f0dd7c460dde52c14f
Connor Todd <connortodd0304@gmail.com>
2020-12-01 16:46:33 +0000

    fetching and rendering all game categories

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
+import axios from "axios";
 
 function Home() {
-  return <div>Home</div>;
+  const [gameCategories, setGameCategories] = React.useState([]);
+
+  React.useEffect(() => {
+    getGameCategories();
+  }, []);
+
+  const getGameCategories = () => {
+    axios
+      .get("https://opentdb.com/api_category.php")
+      .then((response) => setGameCategories(response.data.trivia_categories));
+  };
+
+  return (
+    <div>
+      {gameCategories.map((gameCategory) => (
+        <p>{gameCategory.name}</p>
+      ))}
+      <p>Home</p>
+      <button>Randomize your game </button>
+    </div>
+  );
 }
 
 export default Home;
