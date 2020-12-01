0b76aa6e77f292478e676b39ee49530dc23f8470
Connor Todd <connortodd0304@gmail.com>
2020-12-01 18:03:29 +0000

    added typescript to the home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import axios from "axios";
 
+export interface IGameCategories {
+  id: number;
+  name: string;
+}
+
 function Home() {
-  const [gameCategories, setGameCategories] = React.useState([]);
-  const [selectedGameCategoryId, setSelectedGameCategoryId] = React.useState(0);
-  const [selectedDifficulty, setSelectedDifficulty] = React.useState("easy");
+  const [gameCategories, setGameCategories] = React.useState<IGameCategories[]>([]);
+  const [selectedGameCategoryId, setSelectedGameCategoryId] = React.useState<number>(0);
+  const [selectedDifficulty, setSelectedDifficulty] = React.useState<string>("easy");
 
   React.useEffect(() => {
     getGameCategories();
@@ -27,18 +32,20 @@ function Home() {
       });
   };
 
+
+
   return (
     <div>
       {/* Game Categories dropdown */}
-      <select
-        onChange={(event) => setSelectedGameCategoryId(event.target.value)}
-      >
+      <select onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>  
+        setSelectedGameCategoryId(Number(event.target.value))
+      }>
         {gameCategories.map((gameCategory) => (
           <option value={gameCategory.id}>{gameCategory.name}</option>
         ))}
       </select>
       {/* Difficulty dropdown */}
-      <select onChange={(event) => setSelectedDifficulty(event.target.value)}>
+      <select onChange={(event: React.ChangeEvent<HTMLSelectElement>) => setSelectedDifficulty(event.target.value)}>
         {["easy", "medium", "hard"].map((difficulty) => (
           <option value={difficulty}>{difficulty}</option>
         ))}
