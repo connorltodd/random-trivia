0e628ebce3b64d5d4f6f3b21724bc914965b7888
Connor Todd <connortodd0304@gmail.com>
2020-12-01 23:05:07 +0000

    implemented result page

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import parse from "html-react-parser";
+import { GameContext } from '../contexts/GameContext';
+import { createAnswersList } from '../helpers/createAnswerslist';
 
 function Result() {
-  return <div>Result</div>;
+    let { gameQuestions, points } = React.useContext(GameContext);
+  return (
+    <div>
+     <p>Result</p>
+     <p>Your score was {points}</p>
+     <p>You scored {points / 10} out of 10</p>
+    {gameQuestions.map((gameQuestion: any, index) => (
+      <div key={index}>
+        <p>{parse(gameQuestion.question)}</p>
+        {createAnswersList(gameQuestion.incorrect_answers, gameQuestion.correct_answer)
+          .map((answer, index) => answer === gameQuestion.correct_answer ?
+            <button
+              key={index} 
+              style={{ backgroundColor: "red"}}
+            >
+              {parse(answer)}
+            </button>
+            :
+            <button
+              key={index} 
+            >
+              {parse(answer)}
+            </button>
+          
+          )}
+      </div>
+    ))}
+    </div>
+  );
 }
 
 export default Result;
